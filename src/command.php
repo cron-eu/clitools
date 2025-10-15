ef2e6fbfbd4ce52ccaf5e12b7909cc0007089c0d
Ernesto Baschny <eb@cron.eu>
2025-10-15 12:33:59 +0200

    Fix phar file not finding the VERSION file

--- src/command.php
+++ src/command.php
@@ -25,7 +25,7 @@ error_reporting(E_ALL);
 // Get version from VERSION file using closure
 $getVersion = function() {
     $versionFile = \Phar::running() !== ''
-        ? dirname(Phar::running(false)) . '/VERSION'
+        ? 'phar://' . Phar::running(false) . '/VERSION'
         : dirname(__DIR__) . '/VERSION';
     return trim(file_get_contents($versionFile));
 };
